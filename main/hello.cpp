11fef02bca287d4675883d1655818b3857e22324
Nithin <nithinrk21u@gmail.com>
2025-03-13 10:43:09 +0530

    new commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,12 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "PES2UG22CS372 LIKES CC \n";
   return 0;
 }
